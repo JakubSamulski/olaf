aa2b7f107a45f1927fb6e50cd816f27644a15161
weronika <weronika.lit0@gmail.com>
2024-04-02 23:10:41 +0200

    move to next level

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other){
+        print("Trigger entered");
+
+        if(other.tag == "player"){
+            print("Switching scene to: " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
